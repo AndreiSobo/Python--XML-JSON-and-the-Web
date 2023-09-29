7dd9372f700084683a91fe13650a92f954faeb13
Andrei Sobo <a.sobo1@uni.brighton.ac.uk>
2023-09-29 13:39:50 +0100

    Implemented xml.sax

--- 05_xml_sax/sax_parse_start.py
+++ 05_xml_sax/sax_parse_start.py
@@ -8,16 +8,35 @@ class MyContentHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.slideCount = 0
         self.itemCount = 0
+        self.isInTitle = False
 
     #TODO: Handle startElement
-
+    def startElement(self, tagname, attrs):
+        if tagname == "slideshow":
+            print("Slideshow title is: " + attrs["title"])
+        elif tagname == "item":
+            self.itemCount += 1
+        elif tagname == "slide":
+            self.slideCount += 1
+        elif tagname == "title":
+            self.isInTitle = True
     #TODO: Handle endElement
+    def endElement(self, tagname):
+        if tagname == "title":
+            self.isInTitle = False
 
     #TODO: Handle text data
+    def textData(self, chars):
+        if self.isInTitle:
+            print("title" + chars)
 
     #TODO: Handle startDocument
+    def startDocument(self):
+        print("We starting now!!")
 
     #TODO: Handle endDocument
+    def endDocument(self):
+        print("We finishing up!1")
 
 
 def main():
@@ -28,8 +47,11 @@ def main():
     # remember that Requests auto-decodes our content
     url = "http://httpbin.org/xml"
     result = requests.get(url)
+   
+    
 
     # TODO: call the parseString method on the XML text content received
+    xml.sax.parseString(result.text, handler)
     
 
     # when we're done, print out some interesting results
